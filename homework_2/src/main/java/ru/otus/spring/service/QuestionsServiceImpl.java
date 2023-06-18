e2f76a141affcf5a2efa0c88ae6e4854efab0ddc
Diowelcome <Diowelcome@gmail.com>
2023-06-18 20:03:37 +0300

    Удаление ненужного импорта
    Таки затесался...

--- homework_2/src/main/java/ru/otus/spring/service/QuestionsServiceImpl.java
+++ homework_2/src/main/java/ru/otus/spring/service/QuestionsServiceImpl.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static java.lang.Integer.MAX_VALUE;
 import static java.lang.Math.round;
 
 @Service
