5f89d16140a532fcb92be8fb808dd6a233f44acc
Diowelcome <Diowelcome@gmail.com>
2023-06-19 22:10:39 +0300

    Дополнены тесты
    Новый Test Case: задание несуществующего флага
    Проверка самого текста ответа

--- homework_2/src/test/java/ru/otus/spring/dao/QuestionsDaoSimpleTest.java
+++ homework_2/src/test/java/ru/otus/spring/dao/QuestionsDaoSimpleTest.java
@@ -14,6 +14,7 @@ class QuestionsDaoSimpleTest {
     void shouldReturnRightFlag() {
         String answerWithRightFlag = "Answer with right flag:1";
         Answer answer = QuestionsDaoSimple.parseAnswer(answerWithRightFlag, ':');
+        assertEquals(answer.getAnswer(), answerWithRightFlag.substring(0, answerWithRightFlag.indexOf(':')));
         assertEquals(answer.getRightFlag(), 1);
     }
 
@@ -22,14 +23,25 @@ class QuestionsDaoSimpleTest {
     void shouldReturnWrongFlag() {
         String answerWithWrongFlag = "Answer with wrong flag:0";
         Answer answer = QuestionsDaoSimple.parseAnswer(answerWithWrongFlag, ':');
+        assertEquals(answer.getAnswer(), answerWithWrongFlag.substring(0, answerWithWrongFlag.indexOf(':')));
+        assertEquals(answer.getRightFlag(), 0);
+    }
+
+    @DisplayName("разбор ответа: неверное задание признака правильного ответа")
+    @Test
+    void shouldReturnCorrectWrongFlag() {
+        String answerWithIncorrectFlag = "Answer with wrong flag value:2";
+        Answer answer = QuestionsDaoSimple.parseAnswer(answerWithIncorrectFlag, ':');
+        assertEquals(answer.getAnswer(), answerWithIncorrectFlag.substring(0, answerWithIncorrectFlag.indexOf(':')));
         assertEquals(answer.getRightFlag(), 0);
     }
 
     @DisplayName("разбор ответа: отсутствие признака правильного ответа")
     @Test
     void shouldReturnDefaultWrongFlag() {
-        String answerWithoutFlag = "Answer without flag";
+        String answerWithoutFlag = "Answer with false wrong flag";
         Answer answer = QuestionsDaoSimple.parseAnswer(answerWithoutFlag, ':');
+        assertEquals(answer.getAnswer(), answerWithoutFlag);
         assertEquals(answer.getRightFlag(), 0);
     }
 
